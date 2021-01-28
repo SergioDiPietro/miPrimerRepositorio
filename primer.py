0c293d75555dbd769321fbf43d9b089aa2520733
SergioDiPietro <usr2264@salesianos-lacuesta.net>
2021-01-28 14:08:36 +0000

    Update primer.py
    Añadiendo código python (estoy cambiando el fichero)

--- primer.py
+++ primer.py
@@ -1,2 +1,8 @@
 # Primer fichero
 print ("hola")
+letra = input("dame una letra: ")
+
+print("lo que escribiste fue: ", letra)
+
+numero = int(input("dame un número: "))
+print("lo que escribiste fue: ", numero)
